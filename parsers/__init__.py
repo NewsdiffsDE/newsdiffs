c7a11f6342b810e08fd12299e43b6c64df60d419
Anja Kammer <post@anjakammer.de>
2015-06-13 22:14:44 +0200

    completet n-tv

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -20,6 +20,15 @@ welt.WeltParser
 """
 
 parsers = """
+sueddeutsche.SDParser
+stern.SternParser
+bild.BildParser
+focus.FocusParser
+spiegel.SpiegelParser
+zeit.ZeitParser
+RPOnline.RPOParser
+faz.FAZParser
+n-tv.NTVParser
 welt.WeltParser
 """.split()
 
--- parsers/n-tv.py
+++ parsers/n-tv.py
@@ -5,16 +5,23 @@ from BeautifulSoup import BeautifulSoup, Tag
 class NTVParser(BaseParser):
     domains = ['www.n-tv.de']
 
-    feeder_pat = '^http://www.n-tv.de/(politik|wirtschaft|panorama|technik|wissen)/[a-z]'
+    feeder_pat = '^http://www.n-tv.de/(politik|wirtschaft|panorama|technik|wissen)/.*article\d*'
     feeder_pages = ['http://www.n-tv.de']
 
     def _parse(self, html):
         soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES,
                              fromEncoding='utf-8')
         self.meta = soup.findAll('meta')
-        if soup.find('title').getText().find('Mediathek'):
-            self.real_article = False
-            return
+        # category
+        keywords = self.url.strip('http://www.n-tv.de').replace('/', ',')
+        self.category = self.compute_category(keywords if keywords else '')
+        # Remove any potential "rogue" video articles, that bypass the URL check
+        try:
+            if 'Mediathek' in soup.find('title').getText():
+                self.real_article = False
+                return
+        except:
+            pass
         #article headline
         elt = soup.find('h1', {'class': 'h1'})
         if elt is None:
@@ -25,7 +32,7 @@ class NTVParser(BaseParser):
         author = soup.find('p', {'class': 'author'})
         self.byline = author.getText() if author else ''
         # article date
-        created_at = soup.find('div', {'itemprop': 'date-published'})
+        created_at = soup.find('div', {'itemprop': 'datePublished'})
         self.date = created_at['content'] if created_at else ''
         #article content
         div = soup.find('div', {'class': 'content'})
@@ -36,3 +43,4 @@ class NTVParser(BaseParser):
         map(lambda x: x.extract(), div.findAll('p', {'class': 'author'}))
         self.body = '\n' + '\n\n'.join([x.getText() for x in div.childGenerator()
                                         if isinstance(x, Tag) and x.name == 'p'])
+
