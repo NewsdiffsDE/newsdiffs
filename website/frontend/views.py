838ef1c208845f1e5acb86fedf1b701d585b7ece
catcosmo <krawallmietze@Catarinas-MacBook-Pro.local>
2015-07-15 18:33:49 +0200

    changed version source for article history

--- website/frontend/views.py
+++ website/frontend/views.py
@@ -421,9 +421,9 @@ def article_history(request):
             return HttpResponse('Bug!')
 
     # was article-history
-    rowinfo = get_rowinfo(article)
+    versions = Version.objects.get(article_id=id)
     return render_to_response('article_history.html', {'article':article,
-                                                       'versions':rowinfo,
+                                                       'versions':versions,
             'display_search_banner': came_from_search_engine(request),
                                                        })
 def article_history_feed(request):
