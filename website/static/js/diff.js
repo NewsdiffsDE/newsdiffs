20b0a022177ad7181792f2cd5c274aa742d65323
anjakammer <post@anjakammer.de>
2015-07-27 14:34:06 +0200

    outsourced js

---
+++ website/static/js/diff.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+  var text1 = '{{text1|escapejs}}';
+  var text2 = '{{text2|escapejs}}';
+  $(document).ready(function () {
+    var dmp = new diff_match_patch();
+    dmp.Diff_ShowPara = false;
+    var diff = dmp.diff_main(text1, text2);
+    dmp.diff_cleanupSemantic(diff);
+    $('#compare')[0].innerHTML = dmp.diff_prettyHtml(diff);
+  });
+</script>
